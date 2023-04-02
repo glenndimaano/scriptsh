d97f1b1a848b6bde65f8864d04cc053b38d9690f
glenndimaano <dev.glenndimaano@gmail.com>
2023-04-02 19:33:02 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# scriptsh
+
+## bash script for running homebrew services
+
+### run to make executable
+
+`chmod +x /path/to/file.sh`
+
+### bash_profile
+
+`alias postgresql="cd /path/to/folder && ./postgresql.sh"` <br>
+<qoute><b>command</b> postgresql start | stop</qoute>
+
+`alias mongo="cd /path/to/folder && ./mongo.sh"` <br>
+<qoute><b>command</b> mongo start | stop</qoute>
---
+++ postgresql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Start PostgreSQL 14
+function start_postgresql {
+    brew services start postgresql@14
+    echo "PostgreSQL 14 is running!"
+}
+
+# Stop PostgreSQL 14
+function stop_postgresql {
+    brew services stop postgresql@14
+    echo "PostgreSQL 14 has been stopped."
+}
+
+# Check PostgreSQL status
+function check_postgresql_status {
+    if pg_isready &>/dev/null; then
+        echo "PostgreSQL 14 is running."
+    else
+        echo "PostgreSQL 14 is not running."
+    fi
+}
+
+# Parse command line arguments
+case "$1" in
+    start)
+        start_postgresql
+        ;;
+    stop)
+        stop_postgresql
+        ;;
+    status)
+        check_postgresql_status
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|status}"
+        exit 1
+        ;;
+esac
